0ad72d289dbd2ce9979a1f1f7fc6c28877e658e3
klarahirao <hirao.klara@gmail.com>
2019-02-15 17:22:04 +0100

    Setup and add libraries

--- lib/journal_ex_api/repo.ex
+++ lib/journal_ex_api/repo.ex
@@ -2,4 +2,5 @@ defmodule JournalExApi.Repo do
   use Ecto.Repo,
     otp_app: :journal_ex_api,
     adapter: Ecto.Adapters.Postgres
+  use Scrivener, page_size: 10
 end
--- mix.exs
+++ mix.exs
@@ -19,6 +19,7 @@ defmodule JournalExApi.MixProject do
   # Type `mix help compile.app` for more information.
   def application do
     [
+      applications: [:scrivener_ecto],
       mod: {JournalExApi.Application, []},
       extra_applications: [:logger, :runtime_tools]
     ]
@@ -40,7 +41,12 @@ defmodule JournalExApi.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:guardian, "~> 1.0"},
+      {:comeonin, "~> 5.0"},
+      {:bcrypt_elixir, "~> 2.0"},
+      {:ex_machina, "~> 2.2", only: :test},
+      {:scrivener_ecto, "~> 2.0"}
     ]
   end
 
